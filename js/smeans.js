19a1f31de100f1c6916cea954c6cafbbb2609064
Brian Hann <emailc0bra@gmail.com>
2014-12-18 14:18:19 -0600

    Updates

--- js/smeans.js
+++ js/smeans.js
@@ -1,5 +1,5 @@
 !function(e){if("object"==typeof exports&&"undefined"!=typeof module)module.exports=e();else if("function"==typeof define&&define.amd)define([],e);else{var f;"undefined"!=typeof window?f=window:"undefined"!=typeof global?f=global:"undefined"!=typeof self&&(f=self),f.Smeans=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-/*! smeans - v0.1.4 - 2014-12-18
+/*! smeans - v0.1.5 - 2014-12-18
 * https://github.com/c0bra/smeans.js
 * Copyright (c) 2014 Brian Hann; Licensed MIT */
 
