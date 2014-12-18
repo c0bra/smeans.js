e967d077164c94bf770ab0ad8b8136042bc81a2b
Brian Hann <emailc0bra@gmail.com>
2014-12-18 13:41:42 -0600

    Updates

---
+++ js/docs.js
@@ -0,0 +1,56 @@
+
+angular.module('docsApp', ['ngRoute', 'navData', 'pagesData', 'docs.bootstrap'])
+
+.config(function ($routeProvider) {
+  $routeProvider
+    .when('/doc/:path*', {
+      controller: 'DocsController',
+      templateUrl: function(urlattr) {
+        return '/partials/' + urlattr.path + '.html';
+      },
+      resolve: {
+        $route: function ($route) {
+          return $route;
+        }
+      }
+    })
+    .otherwise('/doc/Tutorial');
+})
+
+.controller('DocsController', function ($log, $scope, DOCS_PAGES, NAV_DATA, $route) {
+  $scope.DOCS_PAGES = DOCS_PAGES;
+  $scope.NAV_DATA = NAV_DATA;
+  $scope.$route = $route;
+
+  this.include = function (path) {
+    $scope.includePath = path;
+  }
+
+  this.getIncludePath = function () {
+    return $scope.includePath
+      ? '/partials/' + $scope.includePath + '.html'
+      : null;
+  };
+
+  this.linkClass = function (doc) {
+    if (!$route.current) { return; }
+
+    var path = doc.path.replace(/\.\//, '');
+    
+    if ($route.current.params.path === path) { return 'active'; }
+  };
+})
+
+.filter('orderObjectBy', function() {
+  return function(items, field, reverse) {
+    var filtered = [];
+    angular.forEach(items, function(item) {
+      filtered.push(item);
+    });
+    filtered.sort(function (a, b) {
+      return (a[field] > b[field] ? 1 : -1);
+    });
+    if(reverse) filtered.reverse();
+    return filtered;
+  };
+});
